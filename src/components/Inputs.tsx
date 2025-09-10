59b708778613b8ef63abea74321a6286511e8407
cotne998 <cotnecincadze998@gmail.com>
2025-09-10 17:00:03 +0400

    error fix: JSX element

--- src/components/Inputs.tsx
+++ src/components/Inputs.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState, useEffect, type JSX } from "react";
 import { diffWords } from "diff";
 import { MainContext } from "../App";
 
